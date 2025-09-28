6e13cef2371e20b9c023f9b9ace4439ee7bfbdf9
fluo10 <fluo10.dev@fireturtle.net>
2025-09-29 07:44:53 +0900

    Add authorization_request.proto

--- core/build.rs
+++ core/build.rs
@@ -2,7 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .extern_path(".tripod_id", "::tripod_id::prost")
         .compile_protos(
-            &["proto/caretta_sync.proto", "proto/caretta_sync.common.proto"],
+            &[
+                "proto/caretta_sync.authorization_request.proto",
+                "proto/caretta_sync.peer_info.proto",
+                "proto/caretta_sync.common.proto"
+            ],
             &["proto", "../tripod-id/proto"]
         )?;
     Ok(())
